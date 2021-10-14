ce764bb54fdbf7da1547078d63d02cbd5a24205a
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-14 17:21:59 -0300

    Add files via upload

---
+++ src/componentes/OlaMundo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './OlaMundo.css'
+
+export default class OlaMundo extends Component {
+    render(){
+        return(
+            <div className="Container">
+                <h1>Olá Mundo React</h1>
+                <h2>Esse é meu componente de classe :]</h2>
+            </div>
+        );
+    }
+}
