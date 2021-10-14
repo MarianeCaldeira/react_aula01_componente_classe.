cdc7541ac6a3d3fe6bfbc615d2056d3724787796
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-14 17:21:15 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import OlaMundo from './components/OlaMundo';
+
+function App() {
+  return (
+    <div className="App">
+      <OlaMundo />
+    </div>
+  );
+}
+
+export default App;
